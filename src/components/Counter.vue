7d5efa983dd88bca6fe68135274a07495d301092
hanyue.ph <hanyue.ph@cainiao.com>
2020-09-28 21:27:50 +0800

    feat: initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{count}}
+    <button @click="plus">+</button>
+    <button @click="subtract">-</button>
+  </div>    
+  {{ template }}
+</template>
+
+<script>
+import setup from '../composition/counter'
+export default {
+  name: 'Counter',
+  props: {
+    defaultValue: Number,
+  },
+  templates: {},
+  setup (props) {
+    const {
+      count,
+      plus,
+      subtract,
+      template,
+    } = setup(props)
+
+    return {
+      template,
+      count,
+      plus,
+      subtract
+    }
+  }
+}
+</script>
